6d51a209c3a9f6ee7eed42d52f95215d035b12c9
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-01-06 10:31:57 -0600

    creating readutils

--- utils/ReadInfos.java
+++ utils/ReadInfos.java
@@ -1,4 +1,25 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReadInfos {
+
+    ArrayList<Record> list;
+
+    //the default constructor
+
+    public ReadInfos(){
+        list = new ArrayList<>();
+    }
+
+    // 1 Add Student
+    public void add(Record record){
+        if(!find(record.getid())){
+            list.add(record);
+        }else{
+
+        }
+
+    }
 }
