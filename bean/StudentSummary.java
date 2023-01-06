9315542b8b86ec0015da3d92eded030c4be7208a
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-01-06 10:00:32 -0600

    defined the bean

--- bean/StudentSummary.java
+++ bean/StudentSummary.java
@@ -1,4 +1,15 @@
 package bean;
 
 public class StudentSummary {
+
+    private int id;
+    private int DOB;
+    private int DOJ;
+    private String name;
+    private String school;
+    private String department;
+    private String subject;
+
+
+
 }
