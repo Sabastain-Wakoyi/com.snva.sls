80ae714e7002afc8df0b342c9e018dc000837afd
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-01-06 10:45:59 -0600

    creating the readutils

--- utils/ReadInfos.java
+++ utils/ReadInfos.java
@@ -18,8 +18,25 @@ public class ReadInfos {
         if(!find(record.getid())){
             list.add(record);
         }else{
+            System.out.println("Student exist already");
 
+        }
+    }
+
+
+    // 2 View all students
+    public void view(){
+        if(list.isEmpty()){
+            System.out.println("No Student found");
+        } for (Record record : list){
+            System.out.println(record.toString());
         }
 
     }
+
+
+
 }
+
+
+
