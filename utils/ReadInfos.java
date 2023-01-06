4809574593e7ae2ae0ff854d0edc098351215c0c
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-01-06 11:29:31 -0600

    completed readutils

--- utils/ReadInfos.java
+++ utils/ReadInfos.java
@@ -2,6 +2,7 @@ package utils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ReadInfos {
 
@@ -23,8 +24,72 @@ public class ReadInfos {
         }
     }
 
+    // 2 update student by id
 
-    // 2 View all students
+    public  boolean find(int id){
+        for (Record l : list){
+            if(l.getid() == id){
+                System.out.println(l);
+                return true;
+            }
+        } return false;
+    }
+
+    //3
+    //Find Record by name
+
+    public Record findRecord(String Name){
+        for (Record l : list){
+            if(l.getName() == Name){
+                return l;
+            }
+        }
+        return  null;
+    }
+
+    //4 update student
+
+    public void update(int id, Scanner input){
+        if(find(id)) {
+            Record rec = findRecord(id);
+            System.out.println("New Student Id");
+            int id = input.nextInt();
+
+            System.out.println("Student Department");
+            String Department = input.nextLine();
+
+            System.out.println("Student Name");
+            String Name = input.nextLine();
+
+            rec.setId(id);
+            rec.setDepartment(Department);
+            rec.setName(Name);
+            System.out.println("Record updated successfully");
+        } else {
+            System.out.println("Student Not Found");
+        }
+
+    }
+
+    // 5
+    //Delete
+    public void delete(int recId){
+        Record recordDel = null;
+        for (Record ll: list){
+            if(ll.getId() == recId){
+                recordDel = ll;
+            }
+        }
+
+        if (recordDel == null){
+            System.out.println("Invalid id");
+        } else {
+            list.remove(recordDel);
+            System.out.println("Record Removed");
+        }
+    }
+
+    // 4 View all students
     public void view(){
         if(list.isEmpty()){
             System.out.println("No Student found");
